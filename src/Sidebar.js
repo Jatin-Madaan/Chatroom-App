170a883a4559d5df9de6d7361e5a0b5a0461cd7a
Deepanshu <jatinmadaan9882@gmail.com>
2020-12-29 15:32:29 +0530

    add new chat prompt in sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -33,6 +33,7 @@ function Sidebar() {
             </div>
 
             <div className="sidebar__chats">
+                <SidebarChat addNewChat />
                 <SidebarChat />
                 <SidebarChat />
             </div>
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,20 +1,31 @@
 import { Avatar } from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
+import "./SidebarChat.css"
 
-function SidebarChat() {
+function SidebarChat({ addNewChat }) {
     const [seed, setSeed] = useState('');
 
     useEffect(()=>{
         setSeed(Math.random(0, 70));
     }, [])
 
-    return (
+    const createChat = () => {
+        const roomName = prompt('please enter name for chat');
+        if (roomName){
+            // do some clever db stuff
+        }
+    }
+    return !addNewChat ? (
         <div className="sidebarChat">
             <Avatar src={`https://i.pravatar.cc/150?${seed}`} />
             <div className="sidebarChat__info">
                 <h2>Room Names</h2>
                 <p>Last message..</p>
             </div>
+        </div>
+    ) : (
+        <div onClick={createChat} className="sidebarChat">
+            <h2>Add new Chat</h2>
         </div>
     )
 }
