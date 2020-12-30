762e7eda99a9720f23fbd2f56492c70296219844
Deepanshu <jatinmadaan9882@gmail.com>
2020-12-31 02:21:37 +0530

    user profile pic is now visible

--- src/Login.js
+++ src/Login.js
@@ -10,6 +10,7 @@ function Login() {
 
     const signIn = () => {
         auth.signInWithPopup(provider).then((result) => {
+            console.log(result);
             dispatch({
                 type: actionTypes.SET_USER,
                 user: result.user,
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,10 +4,12 @@ import "./Sidebar.css"
 import React, { useState, useEffect } from 'react'
 import SidebarChat from './SidebarChat'
 import db from './firebase'
+import { useStateValue } from './StateProvider'
 
 function Sidebar() {
 
     const [rooms, setRooms] = useState([]);
+    const [{ user }, dispatch] = useStateValue();
 
     useEffect(() => {
        const unsubscribe = db.collection('rooms').onSnapshot(snapshot => (
@@ -24,7 +26,7 @@ function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
-                <Avatar />
+                <Avatar src={user?.photoURL}/>
                 <div className="sidebar__headerRight">
                     <IconButton>
                         <DonutLarge />
