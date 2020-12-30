a63d5aaf8ed9c98ba0305cb8da6dd72b9053abab
Deepanshu <jatinmadaan9882@gmail.com>
2020-12-31 03:41:52 +0530

    getting ready to push

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    //get this function from firebase project
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
